02cf07da3f213c049d44dcae55c0d9326d6a9af5
Smital Patel <smitalpatel22@gmail.com>
2021-08-14 22:58:11 +0530

    Tandem Cycle

---
+++ AlgoExpert/src/com/algoexpert/easy/a16TandemCycle.java
@@ -0,0 +1,36 @@
+package com.algoexpert.easy;
+
+import java.util.Arrays;
+
+public class a16TandemCycle {
+    public static void main(String[] args) {
+        System.out.println(findMaxMin(new int[] {5,5,3,9,2}, new int[]{3,6,7,2,1},true));
+        System.out.println(findMaxMin(new int[] {5,5,3,9,2}, new int[]{3,6,7,2,1},false));
+//        System.out.println(findMaxMin(new int[] {0, 1, 21, 33, 45, 45, 61, 71, 72, 73}));
+//        System.out.println(findMaxMin(new int[] {1,5,23,111}));
+//        System.out.println(findMaxMin(new int[] {-1, -2, -3, -7, -17, -27, -18, -541, -8, -7, 7}));
+    }
+
+    public static int findMaxMin(int[] red, int[] blue, boolean fastest) {
+        Arrays.sort(red);
+        Arrays.sort(blue);
+
+        if(fastest) {
+            for (int i = 0, j = red.length - 1; i < j; i++, j--) {
+                int temp = blue[j];
+                blue[j] = blue[i];
+                blue[i] = temp;
+            }
+        }
+
+        int totalSpeed = 0;
+        for(int i=0;i<red.length;i++) {
+            int redSpeed = red[i];
+            int blueSpeed = blue[i];
+            totalSpeed += Math.max(redSpeed,blueSpeed);
+        }
+
+        return totalSpeed;
+
+    }
+}
